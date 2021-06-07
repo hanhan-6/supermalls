3eaba5b2dd39500dbd82eea54afaee1309e128c9
John Han <1114954142@qq.com>
2021-06-07 20:52:11 +0800

    添加路由组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/images/tabbar/home.svg" alt="" />
+        <img slot="item-icon-active" src="./assets/images/tabbar/home_active.svg" alt="" />
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/images/tabbar/category.svg" alt="" />
+        <img slot="item-icon-active" src="./assets/images/tabbar/category_active.svg" alt="" />
+        
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/images/tabbar/shopcart.svg" alt="" />
+        <img slot="item-icon-active" src="./assets/images/tabbar/shopcart_active.svg" alt="" />
+        
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/images/tabbar/profile.svg" alt="" />
+        <img slot="item-icon-active" src="./assets/images/tabbar/profile_active.svg" alt="" />
+       
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabBar from "./views/TabBar.vue";
+import TabBarItem from "./views/TabBarItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TabBar,
+    TabBarItem,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
